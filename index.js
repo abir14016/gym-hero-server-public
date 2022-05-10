a0f7430392e33e3d93ee17166ea5eaff31839c29
abir14016 <abir14016@gmail.com>
2022-05-10 13:52:44 +0600

    database connected to the server

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //require
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -8,6 +9,30 @@ const app = express();
 //middleware
 app.use(cors());
 app.use(express.json());
+
+//connection with database
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.boat8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const inventoryCollection = client.db("gymHero").collection("inventory");
+
+        //load inventory from database to server
+        app.get('/inventory', async (req, res) => {
+            const query = {};
+            const cursor = inventoryCollection.find(query);
+            const inventories = await cursor.toArray();
+            res.send(inventories);
+        });
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('gym hero server is running');
