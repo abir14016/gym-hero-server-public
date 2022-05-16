027e6dec9ef86cdbd056af28f58cf285121732ed
abir14016 <abir14016@gmail.com>
2022-05-17 05:13:02 +0600

    JWT implemented

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //require
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -10,6 +11,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' });
+        }
+        console.log('decoded', decoded);
+        req.decoded = decoded;
+        next();
+    });
+}
+
 //connection with database
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.boat8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -18,9 +35,21 @@ async function run() {
     try {
         await client.connect();
         const inventoryCollection = client.db("gymHero").collection("inventory");
+        const manageInventoryCollection = client.db("gymHero").collection("manageInventories");
         const myInventoryCollection = client.db("gymHero").collection("myInventories");
 
-        //load all inventory data from database to server
+
+
+        //AUTH
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            })
+            res.send({ accessToken });
+        });
+
+        //load only 6 inventories at home page
         app.get('/inventory', async (req, res) => {
             const query = {};
             const cursor = inventoryCollection.find(query);
@@ -28,6 +57,8 @@ async function run() {
             res.send(inventories);
         });
 
+
+        //load all inventory data from database to server
         app.get('/manageinventory', async (req, res) => {
             const query = {};
             const cursor = inventoryCollection.find(query);
@@ -54,7 +85,6 @@ async function run() {
         app.put('/inventory/:id', async (req, res) => {
             const id = req.params.id;
             const updatedUser = req.body;
-            // console.log(typeof (updatedUser.quantity));
             const filter = { _id: ObjectId(id) };
             const options = { upsert: true };
 
@@ -76,16 +106,32 @@ async function run() {
         });
 
 
+        app.get('/manageinventory/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const inventory = await inventoryCollection.findOne(query);
+            res.send(inventory);
+        });
+
+
+
         //myInventories collection API
 
 
-        app.get('/myInventories', async (req, res) => {
+        app.get('/myInventories', verifyJWT, async (req, res) => {
+            const decodedEmail = req?.decoded?.email;
+            console.log(decodedEmail);
             const email = req.query.email;
             console.log(email);
-            const query = { email: email };
-            const cursor = myInventoryCollection.find(query);
-            const myInventories = await cursor.toArray();
-            res.send(myInventories);
+            if (email === decodedEmail) {
+                const query = { email: email };
+                const cursor = myInventoryCollection.find(query);
+                const myInventories = await cursor.toArray();
+                res.send(myInventories);
+            }
+            else {
+                res.status(403).send({ message: 'forbidden access' });
+            }
         });
 
         app.post('/myInventories', async (req, res) => {
